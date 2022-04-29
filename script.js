ff09e5741fe92152d1e009753de92fa9ae451859
Topster_ <85837528+srtopster@users.noreply.github.com>
2022-04-29 17:58:54 -0300

    Mudei bastante coisa

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 player = document.getElementById("player")
 function change(caller){
     player.src = "src/"+caller.innerHTML+".mp3"
-    a = document.getElementsByTagName("a")
-    for (const i of a){
-        if (i.classList.contains("music_red")){
-            i.classList.add("music")
-            i.classList.remove("music_red")
+    for (const a of document.getElementsByTagName("a")){
+        if (a.classList.contains("music_sel")){
+            a.classList.add("music")
+            a.classList.remove("music_sel")
         }
     }
-    caller.classList.add("music_red")
+    caller.classList.add("music_sel")
     caller.classList.remove("music")
     player.load()
     player.play()
